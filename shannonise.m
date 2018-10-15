7824efd3c41348d8c67ed2caffbecca888441698
Gregor Lenz <mail@lenzgregor.com>
2018-10-15 16:54:38 +0200

    rewrote shannonise from scratch

--- shannonise.m
+++ shannonise.m
@@ -1,48 +1,84 @@
-function events = shannonise(recording, scaleFactorTime)
+function events = shannonise(recording, timeStep)
 timeconst = 50000;
-arrayLength = floor((recording.ts(end)-recording.ts(1))/scaleFactorTime/scaleFactorTime);
+arrayLength = floor((recording.ts(end)-recording.ts(1))/timeStep);
 
-events.ts = zeros(1, arrayLength+1);
-events.activityOn = zeros(1, arrayLength);
-events.activityOff = zeros(1, arrayLength);
+events.activityOn = zeros(1, arrayLength+1);
+events.activityOff = zeros(1, arrayLength+1);
 
-l = length(recording.ts);
-index = 1;
-factor = exp(-(scaleFactorTime * scaleFactorTime)/timeconst);
-starttime = floor(recording.ts(1)/scaleFactorTime);
+factor = exp(-timeStep/timeconst);
+starttime = recording.ts(end) - arrayLength * timeStep;
+returnTimestamps = linspace(starttime, recording.ts(end), arrayLength+1);
 
-for event = 1:l
-    rec_ts = floor(recording.ts(event)/scaleFactorTime);
-    scaledTS = rec_ts - mod(rec_ts - starttime, scaleFactorTime);
-    regularTS = starttime + scaleFactorTime * (index - 1);
-    events.ts(index) = regularTS;
-    
-    valOn = events.activityOn(index);
-    valOff = events.activityOff(index);
-    
-    while regularTS < scaledTS
-        valOn = valOn * factor;
-        valOff = valOff * factor;
-        regularTS = regularTS + scaleFactorTime;
-        events.activityOn(index) = valOn;
-        events.activityOff(index) = valOff;
-        events.ts(index) = regularTS;
-        index = index + 1;
-    end
+% ON
+tOn = recording.ts(recording.p == 1);
+recording.activityOn = recording.activityOn(recording.p == 1);
+i = 1;
+while returnTimestamps(i) < tOn(1)
+    events.activityOn(i) = (recording.activityOn(1)-1)/exp(-(tOn(1)-starttime)/timeconst);
+    i = i + 1;
+end
+for j = 1:length(tOn)
+    regularTS = tOn(j);
 
-    if recording.p(event) == 1
-        events.activityOn(index) = recording.activityOn(event);
-        if index > 1
-            events.activityOff(index) = events.activityOff(index-1) * factor;
+    while regularTS > starttime + (i+1) * timeStep
+        if i == 1
+            events.activityOn(i) = recording.activityOn(j-1) * exp(-(starttime - tOn(j-1))/timeconst);
+        else
+            events.activityOn(i) = events.activityOn(i-1) * factor;
         end
+        i = i + 1; 
+    end
+    
+    scaledTS = starttime + (i-1) * timeStep;
+    if scaledTS > regularTS
+        continue
+    elseif scaledTS == regularTS
+        events.activityOn(i) = recording.activityOn(j);
+        i = i + 1;
     else
-        if index > 1
-            events.activityOn(index) = events.activityOn(index-1) * factor;
-        end
-        events.activityOff(index) = recording.activityOff(event);
+        events.activityOn(i) = recording.activityOn(j-1) * exp(-(scaledTS - tOn(j-1))/timeconst);
+        i = i + 1;
     end
 end
-if events.ts(end) == 0
-    events.ts(end) = events.ts(end-1)+scaleFactorTime;
+while i <= arrayLength + 1 && returnTimestamps(i) > tOn(end)
+    events.activityOn(i) = recording.activityOn(end) * exp(-((starttime + i*timeStep) - tOn(end))/timeconst);
+    i = i + 1;
 end
+        
+% OFF
+tOff = recording.ts(recording.p == 0);
+recording.activityOff = recording.activityOff(recording.p == 0);
+i = 1;
+while returnTimestamps(i) < tOff(1)
+    events.activityOff(i) = (recording.activityOff(1)-1)/exp(-(tOff(1)-starttime)/timeconst);
+    i = i + 1;
+end
+for j = 1:length(tOff)
+    regularTS = tOff(j);
+
+    while regularTS > starttime + (i+1) * timeStep
+        if i == 1
+            events.activityOff(i) = recording.activityOff(j-1) * exp(-(starttime - tOff(j-1))/timeconst);
+        else
+            events.activityOff(i) = events.activityOff(i-1) * factor;
+        end
+        i = i + 1;
+    end
+    
+    scaledTS = starttime + (i-1) * timeStep;
+    if scaledTS > regularTS
+        continue
+    elseif scaledTS == regularTS
+        events.activityOff(i) = recording.activityOff(j);
+        i = i + 1;
+    else
+        events.activityOff(i) = recording.activityOff(j-1) * exp(-(scaledTS - tOff(j-1))/timeconst);
+        i = i + 1;
+    end
+end
+while i <= arrayLength + 1 && returnTimestamps(i) > tOff(end)
+    events.activityOff(i) = recording.activityOff(end) * exp(-((starttime + i*timeStep) - tOff(end))/timeconst);
+    i = i + 1;
+end
+events.ts = returnTimestamps;
 end
